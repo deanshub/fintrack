face7fd80ee4a015e89b3eeac00e6fa0fdba469f
Dean Shub <deanshub@gmail.com>
2026-02-13 10:03:51 +0200

    empty project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {
+      "tw-animate-css": "tw-animate-css/dist/tw-animate.css",
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias["tw-animate-css"] =
+      "tw-animate-css/dist/tw-animate.css";
+    return config;
+  },
 };
 
 export default nextConfig;
